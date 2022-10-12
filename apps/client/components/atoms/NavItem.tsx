4a969e3229a64ecddd648ad01e78e97e2f3dbfb0
Lyczeq <lyczeq@gmail.com>
2022-10-12 23:01:52 +0200

    Add basic navbar

---
+++ apps/client/components/atoms/NavItem.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+
+type NavItemProps = {
+  text: string;
+};
+
+export default function NavItem({ text }: NavItemProps) {
+  return (
+    <li className="">
+      <Link href="/exams">
+        <a className="inline-block border py-2 px-4 border-white  transition-all duration-200 hover:border-b-orange-400">
+          {text}
+        </a>
+      </Link>
+    </li>
+  );
+}
+
+export const App: React.FC<{ message: string }> = ({ message }) => (
+  <div>{message}</div>
+);
--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,7 +1,42 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import NavItem from '../components/atoms/NavItem';
 
 const Home: NextPage = () => {
-  return <div>Dinan app</div>;
+  return (
+    <div>
+      <Head>
+        <title>Dinan</title>
+      </Head>
+      <div className="bg-white px-16">
+        <div className="min-h-screen">
+          <header className="py-10 flex justify-between items-center">
+            <Link href="/">
+              <a className="text-4xl  text-orange-400 font-lobster">
+                <h1>dinan</h1>
+              </a>
+            </Link>
+            <div className="flex items-center justify-around">
+              <nav>
+                <ul className="flex items-center gap-6">
+                  <NavItem text="Exams" />
+                  <NavItem text="Created Exams" />
+                  <NavItem text="Certificates" />
+                </ul>
+              </nav>
+              <button
+                className="ml-6 bg-gradient-to-tr from-orange-400 to-yellow-400 px-4 py-2 text-white rounded-md font-bold
+              "
+              >
+                Connect Wallet
+              </button>
+            </div>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
