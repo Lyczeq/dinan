645f7f263c5514a5dd23f5457773268c8248274d
Lyczeq <lyczeq@gmail.com>
2022-10-13 21:17:37 +0200

    feat: add basic pages with customized head

---
+++ apps/client/pages/certificates.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Certificates: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Dinan | Certificates</title>
+      </Head>
+    </>
+  );
+};
+
+export default Certificates;
