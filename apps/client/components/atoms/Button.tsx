47ebbf6468b2c89184cf929b8b24b7cb49bb5119
Lyczeq <lyczeq@gmail.com>
2022-10-26 19:25:45 +0200

    feat: add copying address handler

--- apps/client/components/atoms/Button.tsx
+++ apps/client/components/atoms/Button.tsx
@@ -3,11 +3,15 @@ import React, { ReactNode } from 'react';
 type ButtonProps = {
   children: ReactNode;
   className: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = ({ className, children }: ButtonProps) => {
+const Button = ({ className, children, onClick }: ButtonProps) => {
   return (
-    <button className={`text-white rounded-md py-2 px-2 ${className}`}>
+    <button
+      className={`text-white rounded-md py-2 px-2 ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
--- apps/client/components/atoms/ExamTile.tsx
+++ apps/client/components/atoms/ExamTile.tsx
@@ -1,6 +1,13 @@
+import copy from 'copy-to-clipboard';
 import Button from './Button';
 
 export const ExamTile = () => {
+  const tempAddr = '0x0';
+
+  const handleCopyAddress = () => {
+    copy(tempAddr);
+  };
+
   return (
     <div className="h-72 border-2 px-2 pb-2 border-yellow-400 rounded-md flex flex-col items-center gap-4 justify-between">
       <div className="flex items-center flex-col overflow-hidden ">
@@ -13,7 +20,10 @@ export const ExamTile = () => {
       </div>
       <div className="flex items-center flex-col justify-self-end text-gray-700">
         <p className="">Created 20.11.2022</p>
-        <Button className="bg-yellow-400 p-0 px-2 py-1">
+        <Button
+          className="bg-yellow-400 p-0 px-2 py-1"
+          onClick={handleCopyAddress}
+        >
           <p>Copy address</p>
         </Button>
       </div>
