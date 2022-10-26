0dd906b73e1ceb76bcfe5055334738101a7f1483
Jakub Lyczko <lyczeq@gmail.com>
2022-10-26 22:13:48 +0200

    feat: add simple homepage (#8)

--- apps/client/components/atoms/Logo.tsx
+++ apps/client/components/atoms/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Logo = () => (
   <Link href="/">
-    <a className="text-4xl text-orange-400 font-lobster">
+    <a className="text-5xl text-orange-400 font-lobster">
       <h1>dinan</h1>
     </a>
   </Link>
