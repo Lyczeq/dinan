a378b23fceccaabe9a5215e876323019407a78f3
Lyczeq <lyczeq@gmail.com>
2022-10-26 20:03:01 +0200

    feat: add selectedRoute hihglighting

--- apps/client/components/atoms/NavbarItem.tsx
+++ apps/client/components/atoms/NavbarItem.tsx
@@ -1,5 +1,6 @@
 import React, { AnchorHTMLAttributes, ReactNode } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type NavItemProps = {
   href: string;
@@ -7,10 +8,16 @@ type NavItemProps = {
 } & AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const NavbarItem = ({ children, href }: NavItemProps) => {
+  const { route } = useRouter();
+  const selectedRouteClassName =
+    href === route ? 'font-bold  text-orange-400' : null;
+
   return (
     <li className="">
       <Link href={href}>
-        <a className="inline-block  py-2 px-4 border border-white  transition-all duration-200 hover:border-b-orange-400">
+        <a
+          className={`inline-block  py-2 px-4 border border-white  transition-all duration-200 hover:border-b-orange-400 ${selectedRouteClassName}`}
+        >
           {children}
         </a>
       </Link>
--- apps/client/components/organisms/Navbar.tsx
+++ apps/client/components/organisms/Navbar.tsx
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router';
 import { NavbarItem } from '../atoms/NavbarItem';
 
-export const Navbar = () => (
-  <nav>
-    <ul className="flex items-center gap-6">
-      <NavbarItem href="/exams">Exams</NavbarItem>
-      <NavbarItem href="created-exams">Created Exams</NavbarItem>
-      <NavbarItem href="certificates">Certificates</NavbarItem>
-    </ul>
-  </nav>
-);
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex items-center gap-6">
+        <NavbarItem href="/exams">Exams</NavbarItem>
+        <NavbarItem href="/created-exams">Created Exams</NavbarItem>
+        <NavbarItem href="/certificates">Certificates</NavbarItem>
+      </ul>
+    </nav>
+  );
+};
