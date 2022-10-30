6b916baed654bd5742e11378b595482f5a38ff4f
Lyczeq <lyczeq@gmail.com>
2022-10-30 21:34:17 +0100

    fix: remove Exam validation tests

--- apps/hardhat/test/Exam.test.ts
+++ apps/hardhat/test/Exam.test.ts
@@ -2,13 +2,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-import { Question } from '../types/Exam';
-import {
-  EXAM_DESCRIPTION,
-  EXAM_NAME,
-  MOCK_ANSWERS,
-  MOCK_QUESTIONS,
-} from './constants';
+import { EXAM_DESCRIPTION, EXAM_NAME, MOCK_QUESTIONS } from './constants';
 import { getTimestampFixture } from './utils';
 
 async function deployExamFixture() {
@@ -29,32 +23,6 @@ async function deployExamFixture() {
 
   return { exam, owner, otherAccount };
 }
-
-describe('Validation', () => {
-  it("Shouldn't add Exam with more than 6 answers in a single question.", async () => {
-    const { exam } = await loadFixture(deployExamFixture);
-
-    const basicAnswer = MOCK_ANSWERS.at(0)!;
-
-    const answersArray = new Array(7).fill(0).map((_, idx) => ({
-      ...basicAnswer,
-      id: idx,
-    }));
-
-    const questionWithTooManyAnswers: Question = {
-      id: 1,
-      description: EXAM_DESCRIPTION,
-      header: EXAM_NAME,
-      answers: answersArray,
-    };
-
-    await expect(
-      exam.addQuestion(questionWithTooManyAnswers)
-    ).to.be.revertedWith(
-      'The maximum number of answers you can add to a single question is 6.'
-    );
-  });
-});
 
 describe('Exam tests', () => {
   it('Creates a simple Exam without adding questions.', async () => {
