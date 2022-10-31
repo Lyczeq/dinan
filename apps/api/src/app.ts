c05a261599dc1512add247dedd2608febee15d05
Lyczeq <lyczeq@gmail.com>
2022-10-31 20:10:03 +0100

    feat: add prisma disconnext in app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+
 import { router as exams } from './routes/exams';
 import { env } from './config';
+import prisma from './prisma';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/exams', exams);
 
-app.listen(env.PORT, () =>
-  console.log(`🚀 Server ready at http://localhost:${env.PORT}`)
-);
+const main = async () => {
+  app.listen(env.PORT, () =>
+    console.log(`🚀 Server ready at http://localhost:${env.PORT}`)
+  );
+};
+
+main().catch(e => {
+  console.log(e);
+  prisma.$disconnect;
+});
