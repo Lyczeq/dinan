9e1420da880ee0c4217caa6e52c41383c0a0b06f
Lyczeq <lyczeq@gmail.com>
2022-11-06 12:04:24 +0100

    feat: add test for exam participation that checks a revert

--- apps/hardhat/test/ExamController.test.ts
+++ apps/hardhat/test/ExamController.test.ts
@@ -2,7 +2,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { isAddress } from 'ethers/lib/utils';
-import { EXAM_NAME, EXAM_SYMBOL } from './constants';
+import { EXAM_NAME, EXAM_SYMBOL, EXAM_ADDRESS } from './constants';
 
 async function deployExamControllerFixture() {
   const [owner, otherAccount] = await ethers.getSigners();
@@ -14,22 +14,35 @@ async function deployExamControllerFixture() {
 }
 
 describe('ExamController tests', () => {
-  it("Creates simple Exam and checks it's data", async () => {
-    const { examController, owner } = await loadFixture(
-      deployExamControllerFixture
-    );
-    await examController.addExam(EXAM_NAME, EXAM_SYMBOL);
+  describe('Exam creation', () => {
+    it("Creates simple Exam and checks it's data", async () => {
+      const { examController } = await loadFixture(deployExamControllerFixture);
+      await examController.addExam(EXAM_NAME, EXAM_SYMBOL);
 
-    const exams = await examController.getExams();
-    const firstExamInArray = exams.at(0);
-    expect(firstExamInArray?.name).equal(EXAM_NAME);
+      const exams = await examController.getExams();
+      const firstExamInArray = exams.at(0);
+      expect(firstExamInArray?.name).equal(EXAM_NAME);
+    });
+
+    it("Expects event to be emited after exam's creation", async () => {
+      const { examController, owner } = await loadFixture(
+        deployExamControllerFixture
+      );
+
+      await expect(examController.addExam(EXAM_NAME, EXAM_SYMBOL))
+        .to.emit(examController, 'NewExamCreation')
+        .withArgs(isAddress, owner.address);
+    });
   });
-  it('Expects event to be emited', async () => {
-    const { examController, owner } = await loadFixture(
-      deployExamControllerFixture
-    );
-    await expect(examController.addExam(EXAM_NAME, EXAM_SYMBOL))
-      .to.emit(examController, 'NewExamCreation')
-      .withArgs(isAddress, owner.address);
+
+  describe('Exam participation', () => {
+    it("Expects revert with the message that exam doesn't exist", async () => {
+      const { examController } = await loadFixture(deployExamControllerFixture);
+      await expect(
+        examController.manageExamParticipation(EXAM_ADDRESS)
+      ).to.be.revertedWith("Exam with the provided address doesn't exist.");
+    });
+
+
   });
 });
