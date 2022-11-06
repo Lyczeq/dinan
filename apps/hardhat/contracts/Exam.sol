d9b7eafcb1636c12d94e40a7b92043b87d67e35e
Lyczeq <lyczeq@gmail.com>
2022-11-06 15:06:03 +0100

    feat: add Base64 library

--- apps/hardhat/contracts/Exam.sol
+++ apps/hardhat/contracts/Exam.sol
@@ -3,17 +3,20 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import {Base64} from "../libraries/Base64.sol";
 
 contract Exam is ERC721 {
     // name() method returns Exam's name
     // symbol() method returns Exam's symbol
 
+    using Counters for Counters.Counter;
     struct ExamParticipation {
         bool isFinished;
         uint8 score;
         bool hasStarted;
     }
 
+    Counters.Counter private _tokenIds;
     address private creatorAddress;
     address private examControllerAddress;
     address private backendAddress;
@@ -27,22 +30,6 @@ contract Exam is ERC721 {
     ) ERC721(_name, _symbol) {
         creatorAddress = _creatorAddress;
         examControllerAddress = _examControllerAddress;
-    }
-
-    modifier isExamControllerAddress() {
-        require(
-            msg.sender == examControllerAddress,
-            "Your address isn't the ExamController Contract address."
-        );
-        _; // function code
-    }
-
-    modifier isBackendAddress() {
-        require(
-            msg.sender == backendAddress,
-            "Your address isn't the backend address."
-        );
-        _; // function code
     }
 
     function participateInExam(address participantAddress)
@@ -65,6 +52,22 @@ contract Exam is ERC721 {
             score,
             true
         );
+    }
+
+    modifier isExamControllerAddress() {
+        require(
+            msg.sender == examControllerAddress,
+            "Your address isn't the ExamController Contract address."
+        );
+        _; // function code
+    }
+
+    modifier isBackendAddress() {
+        require(
+            msg.sender == backendAddress,
+            "Your address isn't the backend address."
+        );
+        _; // function code
     }
 
     function getParticipantResult(address _userAddress)
