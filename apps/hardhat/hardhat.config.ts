fd45ba4111b164c22ca0a5eb15541b43cdecd5f1
Lyczeq <lyczeq@gmail.com>
2022-11-08 22:24:33 +0100

    feat: add packages to work with envs in hardhat. Add goerli config in hardhat config

--- apps/hardhat/hardhat.config.ts
+++ apps/hardhat/hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { cleanEnv, str } from 'envalid';
+import { default as dotenv } from 'dotenv';
+
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  GOERLI_PRIVATE_KEY: str(),
+  ALCHEMY_URL: str(),
+});
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: env.ALCHEMY_URL,
+      accounts: [env.GOERLI_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
