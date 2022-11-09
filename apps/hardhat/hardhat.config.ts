5dc69cac721655a7c8e0ad5522a86c9daf29aff9
Lyczeq <lyczeq@gmail.com>
2022-11-09 20:25:11 +0100

    feat: change hardhatconfig to use polygon mumbai instead of goerli

--- apps/hardhat/hardhat.config.ts
+++ apps/hardhat/hardhat.config.ts
@@ -1,21 +1,22 @@
 import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
 import { cleanEnv, str } from 'envalid';
 import { default as dotenv } from 'dotenv';
+import '@nomicfoundation/hardhat-toolbox';
 
 dotenv.config();
 
 const env = cleanEnv(process.env, {
-  GOERLI_PRIVATE_KEY: str(),
+  PRIVATE_KEY: str(),
   ALCHEMY_URL: str(),
 });
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
-    goerli: {
-      url: env.ALCHEMY_URL,
-      accounts: [env.GOERLI_PRIVATE_KEY!],
+    hardhat: {},
+    matic: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [env.PRIVATE_KEY],
     },
   },
 };
