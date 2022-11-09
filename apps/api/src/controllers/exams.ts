8fadb5add679948f51fa310cc0fd1f2c2aed7ac7
Lyczeq <lyczeq@gmail.com>
2022-11-09 22:57:36 +0100

    feat: add getting single exam and participating in exam

--- apps/api/src/controllers/exams.ts
+++ apps/api/src/controllers/exams.ts
@@ -1,6 +1,7 @@
 import { Exam, Question, Answer } from '@prisma/client';
 import { Request, Response } from 'express';
 import prisma from '../prisma';
+import { exclude } from './helpers';
 
 export const getAllExams = async (req: Request, res: Response) => {
   try {
@@ -71,6 +72,79 @@ export const addExam = async (req: Request, res: Response) => {
     res.send(updatedExam);
   } catch (error) {
     console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+export const getSingleExam = async (req: Request, res: Response) => {
+  const examAddress = req.params.address;
+  try {
+    const exam = await prisma.exam.findUnique({
+      where: {
+        address: examAddress,
+      },
+    });
+    if (!exam) return res.sendStatus(404);
+
+    res.statusCode = 200;
+    return res.json({ exam });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
+
+export const getExamsQuestionsAndAnswers = async (
+  req: Request,
+  res: Response
+) => {
+  const participantAddress = req.headers.authorization?.split(' ').at(1);
+  const examAddress = req.params.address;
+
+  try {
+    const examParticipation = await prisma.examParticipation.findFirst({
+      where: {
+        examAddress,
+        participantAddress,
+      },
+    });
+
+    if (!examParticipation) return res.sendStatus(404);
+
+    const examToParticipateWithIsCorrectProperties =
+      await prisma.exam.findUnique({
+        include: {
+          questions: {
+            include: {
+              answers: true,
+            },
+          },
+        },
+        where: {
+          address: examAddress,
+        },
+      });
+
+    const examToParicipate = {
+      ...examToParticipateWithIsCorrectProperties,
+      questions: examToParticipateWithIsCorrectProperties?.questions.map(
+        question => {
+          return {
+            ...question,
+            answers: question.answers.map(answer =>
+              exclude(answer, 'isCorrect')
+            ),
+          };
+        }
+      ),
+    };
+
+    res.statusCode = 200;
+    return res.json({
+      examAddress,
+      participantAddress,
+      exam: examToParicipate,
+    });
+  } catch (error) {
     res.sendStatus(500);
   }
 };
--- apps/api/src/routes/exams.ts
+++ apps/api/src/routes/exams.ts
@@ -1,7 +1,14 @@
 import express from 'express';
-import { getAllExams, addExam } from '../controllers/exams';
+import {
+  getAllExams,
+  addExam,
+  getSingleExam,
+  getExamsQuestionsAndAnswers,
+} from '../controllers/exams';
 
 export const router = express.Router();
 
 router.route('/').get(getAllExams);
 router.route('/').post(addExam);
+router.route('/:address').get(getSingleExam);
+router.route('/:address/participate').get(getExamsQuestionsAndAnswers);
