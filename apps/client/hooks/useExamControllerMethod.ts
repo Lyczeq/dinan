eca8cfac368f127ba1a22792cae17efd26284469
Lyczeq <lyczeq@gmail.com>
2022-11-13 20:26:23 +0100

    feat: change hook to use metamask, adjust add-exam page

--- apps/client/hooks/useExamControllerMethod.ts
+++ apps/client/hooks/useExamControllerMethod.ts
@@ -3,14 +3,13 @@ import { Contract } from 'ethers';
 import { Interface } from 'ethers/lib/utils';
 import ExamController from '../utils/ExamController.json';
 
-const addExamMetodName = 'addExam';
 const examControllerAddress = '0xa86f4c00F1CFe4E3446bD0DE788A81BeBF567F9e';
 
-export const useAddExam = () => {
+export const useExamControllerMethod = (methodName: string) => {
   const { send, state, events } = useContractFunction(
     new Contract(examControllerAddress, new Interface(ExamController.abi)),
-    addExamMetodName
+    methodName
   );
 
-  return { addExam: send, state, events };
+  return { send, state, events };
 };
