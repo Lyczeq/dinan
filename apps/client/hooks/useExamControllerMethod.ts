015635973da01f39bf753354a8d7677772abe03e
Lyczeq <lyczeq@gmail.com>
2022-11-15 23:56:01 +0100

    uncomment hook code

--- apps/client/hooks/useExamControllerMethod.ts
+++ apps/client/hooks/useExamControllerMethod.ts
@@ -1,12 +1,12 @@
-// import { getNewExamControllerContract } from '@dinan/contracts/examController/index';
+import { getNewExamControllerContract } from '@dinan/contracts/examController/index';
 import { useContractFunction } from '@usedapp/core';
 
 export const useExamControllerMethod = (methodName: string) => {
-  // const examControllerContract = getNewExamControllerContract();
-  // const { send, state, events } = useContractFunction(
-  //   examControllerContract,
-  //   methodName
-  // );
+  const examControllerContract = getNewExamControllerContract();
+  const { send, state, events } = useContractFunction(
+    examControllerContract,
+    methodName
+  );
 
-  return { send: () => {}, state: '', events: [] };
+  return { send, state, events };
 };
