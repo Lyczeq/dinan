4d3d1b4a245743944bab8ee0b8f2f24a04751352
Lyczeq <lyczeq@gmail.com>
2022-11-20 19:26:55 +0100

    feat: add endpoint to fetch user's nfts with alchemy API

--- apps/client/pages/certificates.tsx
+++ apps/client/pages/certificates.tsx
@@ -1,12 +1,32 @@
+import { useEthers } from '@usedapp/core';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useQuery } from 'react-query';
+
+const fetchUserCertificates = async (userAddress: string) => {
+  const response = await fetch(
+    `http://localhost:8000/api/v1/users/${userAddress}/certificates`
+  );
+
+  const data = response.json();
+  return data;
+};
 
 const Certificates: NextPage = () => {
+  const { account } = useEthers();
+  const { data, status } = useQuery('certificates', {
+    queryFn: () => fetchUserCertificates(account as string),
+    enabled: !!account,
+  });
+
+  
+
   return (
     <>
       <Head>
         <title>Dinan | Certificates</title>
       </Head>
+      <p>{JSON.stringify(data)}</p>
     </>
   );
 };
---
+++ packages/types/Nft.ts
@@ -0,0 +1,20 @@
+export type Nft = {
+  contract: {
+    address: string;
+  };
+  id: {
+    tokenId: string;
+  };
+  metadata: Metadata;
+  contractMetadata: ContractMetadata;
+};
+
+type Metadata = {
+  image: string;
+  name: string;
+  description: string;
+};
+
+type ContractMetadata = {
+  symbol: string;
+};
