b453ef1ad9274caac6166b5a13a4aa10b7aa4b3e
Lyczeq <lyczeq@gmail.com>
2022-11-20 11:47:53 +0100

    move Table component to organisms

--- apps/client/pages/created-exams.tsx
+++ apps/client/pages/created-exams.tsx
@@ -1,11 +1,44 @@
+import { useEthers } from '@usedapp/core';
+import { ErrorMessage } from 'components/atoms/ErrorMessage';
 import { ExamTile } from 'components/atoms/ExamTile';
+import { Loader } from 'components/atoms/Loader';
 import { SearchInput } from 'components/atoms/SearchInput';
-import { Table } from 'components/molecules/Table/Table';
+import { Table } from 'components/organisms/Table/Table';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+const fetchExamsCreatedBySpecificUser = async (userAddress: string) => {
+  const response = await fetch(
+    `http://localhost:8000/api/v1/users/${userAddress}/exams`
+  );
+  return await response.json();
+};
 
 const CreatedExams: NextPage = () => {
+  const { account } = useEthers();
+  const { data, status } = useQuery('created-exams', {
+    queryFn: () => fetchExamsCreatedBySpecificUser(account as string),
+    enabled: !!account,
+  });
+  const [searchInput, setSearchInput] = useState('');
+
+  const filterExams = (exam: any) => {
+    return (
+      exam.address.toLowerCase().includes(searchInput.toLowerCase()) ||
+      exam.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  };
+
+  const handleSearchExam = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const filteredExams = (data?.exams ?? []).filter(filterExams);
+
+  console.log(status);
   return (
     <>
       <Head>
@@ -13,7 +46,7 @@ const CreatedExams: NextPage = () => {
       </Head>
       <Table>
         <Table.Header>
-          <SearchInput />
+          <SearchInput onChange={handleSearchExam} />
           <Link
             href="add-exam"
             className="text-yellow-400 hover:text-orange-400 bold font-bold border-b-2 border-yellow-400 hover:border-orange-400"
@@ -21,11 +54,15 @@ const CreatedExams: NextPage = () => {
             Add new Exam
           </Link>
         </Table.Header>
-        <Table.Content>
-          <ExamTile />
-          <ExamTile />
-          <ExamTile />
-        </Table.Content>
+        <ErrorMessage isError={status === 'error'} />
+        <Loader isLoading={status === 'loading'} />
+        {status === 'success' && (
+          <Table.Content>
+            {filteredExams.map((exam: any) => (
+              <ExamTile key={exam.address} exam={exam} />
+            ))}
+          </Table.Content>
+        )}
       </Table>
     </>
   );
