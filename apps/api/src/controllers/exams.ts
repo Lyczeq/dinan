9b880ddb3b870d3fefc4d95d53f4d7a257a4d885
Lyczeq <lyczeq@gmail.com>
2022-11-20 12:29:49 +0100

    feat: add API endpoint to fetch created exams

--- apps/api/src/controllers/exams.ts
+++ apps/api/src/controllers/exams.ts
@@ -225,3 +225,26 @@ export const compareParticipantAnswers = async (
     });
   } catch (error) {}
 };
+
+export const getExamsCreatedBySpecificUser = async (
+  req: Request,
+  res: Response
+) => {
+  const userAddress = req.params.address;
+
+  try {
+    const exams = await prisma.exam.findMany({
+      where: {
+        creatorAddress: {
+          mode: 'insensitive',
+          equals: userAddress,
+        },
+      },
+    });
+
+    res.statusCode = 200;
+    res.send({
+      exams,
+    });
+  } catch (e) {}
+};
