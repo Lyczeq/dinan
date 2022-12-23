006c433748d350b376dd14ffaa97322a40c79ac3
Lyczeq <lyczeq@gmail.com>
2022-12-23 21:52:21 +0100

    fix: update tsconfig, env config

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -4,7 +4,7 @@ import { default as dotenv } from 'dotenv';
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  PORT: port(),
+  POSTGRES_PORT: port(),
   DATABASE_URL: str(),
   PRIVATE_KEY: str(),
   ALCHEMY_HTTPS_LINK: str(),
